bf4a547ead9e5f773f00b642196f45bc75d17d58
ionscorobogaci <i.scorobogaci@payingit.nl>
2022-04-07 12:12:03 +0300

    reset password update

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Auth} from "aws-amplify";
 import {Router} from "@angular/router";
 import {noop} from "rxjs";
+import {MatSnackBar, MatSnackBarRef, SimpleSnackBar} from "@angular/material/snack-bar";
+import {AppOverlayContainer} from "../services/app-overlay-container";
 
 @Component({
   selector: 'app-reset-password',
@@ -10,45 +12,64 @@ import {noop} from "rxjs";
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
-
-  public passwordResetErrorMessage: string
-
+  private emptyString = ''
+  private snackbarRef: MatSnackBarRef<SimpleSnackBar> | undefined;
   form: FormGroup = new FormGroup({
-    previousPassword: new FormControl(''),
-    newPassword: new FormControl(''),
-    confirmNewPassword: new FormControl(''),
+    previousPassword: new FormControl(this.emptyString, Validators.required),
+    newPassword: new FormControl(this.emptyString, Validators.required),
+    confirmNewPassword: new FormControl(this.emptyString, Validators.required),
   });
 
-  constructor(private router: Router) {
-    this.passwordResetErrorMessage = '';
+  constructor(private router: Router,
+              private snackBar: MatSnackBar,
+              private appOverlayContainer: AppOverlayContainer) {
   }
 
   ngOnInit(): void {
   }
 
-  public resetPassword(): void {
-    this.passwordResetErrorMessage = '';
+  public resetPassword(container: HTMLElement): void {
 
-    if (this.form.controls['newPassword'].value !== this.form.controls['confirmNewPassword'].value) {
-      this.passwordResetErrorMessage = 'Passwords mismatch. Please try again';
-      return
+    if (this.form.valid) {
+      if (this.form.controls['newPassword'].value !== this.form.controls['confirmNewPassword'].value) {
+        this.displaySnack(container, 'Passwords mismatch. Please try again', 'Got It')
+        return
+      }
+      Auth.signIn(localStorage.getItem("username")!, this.form.controls['previousPassword'].value)
+        .then(user => {
+          if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+            Auth.completeNewPassword(user, this.form.controls['newPassword'].value).then(() => {
+              this.router.navigate(['home']).then(noop)
+            }).catch(error => {
+              console.log("Error on completing password reset", error);
+              this.displaySnack(container, error, 'Got It')
+            });
+          } else {
+            this.displaySnack(container, 'Please contact your system administrator', 'Got It')
+          }
+        }).catch(error => {
+        console.log("Error on sign in", error);
+        this.displaySnack(container, 'Please contact your system administrator', 'Got It')
+      });
     }
-    Auth.signIn(localStorage.getItem("username")!, this.form.controls['previousPassword'].value)
-      .then(user => {
-        if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
-          Auth.completeNewPassword(user, this.form.controls['newPassword'].value).then(() => {
-            this.router.navigate(['home']).then(noop)
-          }).catch(error => {
-            console.log("Error on completing password reset", error);
-            this.passwordResetErrorMessage = error;
-          });
-        } else {
-          this.passwordResetErrorMessage = 'Please contact your system administrator';
-        }
-      }).catch(error => {
-      console.log("Error on sign in", error);
-      this.passwordResetErrorMessage = 'Please contact your system administrator';
-    });
+  }
+
+  private displaySnack(overlayContainerWrapper: HTMLElement, message: string, action: string): void {
+    if (overlayContainerWrapper === null) {
+      return;
+    }
+
+    if (this.snackbarRef) {
+      this.snackbarRef.dismiss();
+    }
+
+    setTimeout(() => {
+      this.appOverlayContainer.appendToCustomWrapper(overlayContainerWrapper);
+
+      this.snackbarRef = this.snackBar.open(message, action, {
+        duration: 0
+      });
+    }, 100);
 
   }
 
