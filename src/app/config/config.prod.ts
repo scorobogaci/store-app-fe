7734a4f26f1571eef0cbf473cbacb99191218c20
ionscorobogaci <i.scorobogaci@payingit.nl>
2022-04-26 22:26:54 +0300

    prod stack update

--- src/app/config/config.prod.ts
+++ src/app/config/config.prod.ts
@@ -3,8 +3,8 @@ import {Config} from "./config-definition";
 export const CONFIG_PROD: Config = {
   aws: {
     authenticationFlowType: 'USER_PASSWORD_AUTH',
-    userPoolId: 'us-east-1_E3uZir1cn',
-    userPoolWebClientId: '61cqmeaf94s4ihn1usuuktv9pc',
+    userPoolId: 'us-east-1_xIaH0zt96',
+    userPoolWebClientId: '3sce1vehsrrp5va9i0u4qj43kb',
     identityPoolId:'us-east-1:b454a0fa-6b35-4a2f-a2f0-cc3e9f2cac13',
     region: 'us-east-1'
   },
