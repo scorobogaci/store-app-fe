c45fad16bec675f7b0a81d5380050d2028c66e49
ionscorobogaci <i.scorobogaci@payingit.nl>
2022-04-27 15:36:04 +0300

    just redeployed api

--- src/app/config/config.dev.ts
+++ src/app/config/config.dev.ts
@@ -8,5 +8,5 @@ export const CONFIG_DEV: Config = {
     identityPoolId: 'us-east-1:2e3d5282-0b30-4864-bc07-129b06762e6d',
     region: 'us-east-1'
   },
-  baseApiUrl: 'https://dn0ygv2nzc.execute-api.us-east-1.amazonaws.com/prod'
+  baseApiUrl: 'https://wutvyvqpc7.execute-api.us-east-1.amazonaws.com/prod'
 };
